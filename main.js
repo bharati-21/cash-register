c106fb5994f1726901a84f9d8da759ef8c3da022
Bharati Subramanian <bharatisharada@gmail.com>
2021-07-24 22:36:07 +0530

    added clear button

--- main.js
+++ main.js
@@ -21,14 +21,17 @@ const sectionCashReturnOutput = document.querySelector('.cash-return-output');
 
 const btnSubmit = document.querySelector('.btn-submit');
 const btnCashSubmit = document.querySelector('.btn-submit-cash')
+const btnClear = document.querySelector('.btn-clear')
 
 const notes = [2000, 500, 100, 20, 10, 5, 1];
+
+billAmount.focus();
 
 btnSubmit.addEventListener('click', handleBillAmountSubmit);
 function handleBillAmountSubmit(e) {
     const billAmountValue = billAmount.value;
 
-    if(billAmountValue === ""  || billAmountValue === null) {
+    if(billAmountValue === ""  || parseInt(billAmountValue) === 0 || billAmountValue === null) {
         displayErrorMessage(true, 'Please enter a valid bill amount', sectionBillAmount);
     }
     else {
@@ -36,6 +39,8 @@ function handleBillAmountSubmit(e) {
         
         changeDisplay(btnSubmit, false);
         changeDisplay(sectionCashAmountInput, true);
+
+        cashAmount.focus();
     }
 }
 
@@ -46,89 +51,96 @@ function handleCashSubmit(e) {
     const cashAmountInput = cashAmount.value;
     const billAmountValue = billAmount.value;
 
-    if (cashAmountInput === "" || billAmountValue === ""  || cashAmountInput === null || billAmountValue === null) {
-        displaySuccessMessage(false, "", sectionCashAmountInput);
+    if ( cashAmountInput === "" || billAmountValue === "" ||parseInt(billAmountValue) === 0 || cashAmountInput === null || billAmountValue === null) {
+      displaySuccessMessage(false, "", sectionCashAmountInput);
 
-        displayErrorMessage(true, "Please enter a valid bill and cash amount to continue", sectionCashAmountInput);
+      displayErrorMessage( true, "Please enter a valid bill and cash amount to continue", sectionCashAmountInput);
 
-        changeDisplay(sectionCashReturnOutput, false);
+      changeDisplay(sectionCashReturnOutput, false);
     } 
     else {
-        displayErrorMessage(false, "", sectionCashAmountInput);
-        changeDisplay(sectionCashReturnOutput, false);
+      displayErrorMessage(false, "", sectionCashAmountInput);
+      changeDisplay(sectionCashReturnOutput, false);
 
-        const bill = parseInt(Math.round(billAmountValue, 0));
-        const cash = parseInt(Math.round(cashAmountInput, 0));
+      const bill = parseInt(Math.round(billAmountValue, 0));
+      const cash = parseInt(Math.round(cashAmountInput, 0));
 
-        console.log(bill, cash);
+      console.log(bill, cash);
 
-        if(cash < bill) {
-            displaySuccessMessage(false, "", sectionCashAmountInput);
+      if (cash < bill) {
+        displaySuccessMessage(false, "", sectionCashAmountInput);
 
-            displayErrorMessage(true, "Cash paid is less than bill. Please enter the right amount", sectionCashAmountInput);
+        displayErrorMessage(
+          true,
+          "Cash paid is less than bill. Please enter the right amount",
+          sectionCashAmountInput
+        );
+      } else if (cash === bill) {
+        displaySuccessMessage(
+          true,
+          "No amount should be returned.",
+          sectionCashAmountInput
+        );
+      } else {
+        displaySuccessMessage(false, "", sectionCashAmountInput);
 
-            
+        changeDisplay(sectionCashAmountInput, true);
+
+        changeDisplay(sectionCashReturnOutput, true);
+
+        let change = cash - bill;
+        let numNotes = [];
+        if (change >= 2000) {
+          numNotes[0] = parseInt(change / 2000);
+          change -= 2000 * numNotes[0];
         }
-        else if(cash === bill) {
-            displaySuccessMessage(true, "No amount should be returned.", sectionCashAmountInput);
+
+        if (change >= 500) {
+          numNotes[1] = parseInt(change / 500);
+          change -= 500 * numNotes[1];
         }
-        else {
-            displaySuccessMessage(false, "", sectionCashAmountInput);
 
-            changeDisplay(sectionCashAmountInput, true);
-
-            changeDisplay(sectionCashReturnOutput, true);
-
-            let change = cash - bill;
-            let numNotes = [];
-            if(change >= 2000) {
-                numNotes[0] = parseInt(change/2000);
-                change-=(2000*numNotes[0]);
-            }
-
-            if(change >= 500) {
-                numNotes[1] = parseInt(change / 500);
-                change -= (500 * numNotes[1]);
-            }
-
-            if(change >= 100) {
-                numNotes[2] = parseInt(change / 100);
-                change -= (100 * numNotes[2]);
-            }
-
-            if(change >= 20) {
-                numNotes[3] = parseInt(change / 20);
-                change -= (20 * numNotes[3]);
-            }
-
-            if(change >= 10) {
-                numNotes[4] = parseInt(change / 10);
-                change -= (10 * numNotes[4]);
-            }
-
-            if(change >= 5) {
-                numNotes[5] = parseInt(change / 5);
-                change -= (5 * numNotes[5]);
-            }
-
-            if(change >= 1) {
-                numNotes[6] = parseInt(change / 1);
-                change -= (1 * numNotes[6]);
-            }
-
-            console.log(numNotes);
-
-            twoThousandNotes.textContent = numNotes[0];
-            fiveHundredNotes.textContent = numNotes[1];
-            hundredNotes.textContent = numNotes[2];
-            twentyNotes.textContent = numNotes[3];
-            tenNotes.textContent = numNotes[4];
-            fiveNotes.textContent = numNotes[5];
-            oneCoin.textContent = numNotes[6];
+        if (change >= 100) {
+          numNotes[2] = parseInt(change / 100);
+          change -= 100 * numNotes[2];
         }
+
+        if (change >= 20) {
+          numNotes[3] = parseInt(change / 20);
+          change -= 20 * numNotes[3];
+        }
+
+        if (change >= 10) {
+          numNotes[4] = parseInt(change / 10);
+          change -= 10 * numNotes[4];
+        }
+
+        if (change >= 5) {
+          numNotes[5] = parseInt(change / 5);
+          change -= 5 * numNotes[5];
+        }
+
+        if (change >= 1) {
+          numNotes[6] = parseInt(change / 1);
+          change -= 1 * numNotes[6];
+        }
+
+        console.log(numNotes);
+
+        twoThousandNotes.textContent = numNotes[0];
+        fiveHundredNotes.textContent = numNotes[1];
+        hundredNotes.textContent = numNotes[2];
+        twentyNotes.textContent = numNotes[3];
+        tenNotes.textContent = numNotes[4];
+        fiveNotes.textContent = numNotes[5];
+        oneCoin.textContent = numNotes[6];
+      }
     }
 }
 
+
+
+btnClear.addEventListener('click', clearFields);
 
 function displayErrorMessage(display, messageText, section) {
     if(display) {
@@ -178,4 +190,27 @@ function displaySuccessMessage(display, messageText, section) {
             section.removeChild(message);
         }
     }
+}
+
+
+function clearFields(e) {
+  cashAmount.value = "";
+  billAmount.value = "";
+
+  twoThousandNotes.textContent = "";
+  fiveHundredNotes.textContent = "";
+  hundredNotes.textContent = "";
+  twentyNotes.textContent = "";
+  tenNotes.textContent = "";
+  fiveNotes.textContent = "";
+  oneCoin.textContent = "";
+
+  displayErrorMessage(false, "", sectionBillAmount);
+  displaySuccessMessage(false, "", sectionCashAmountInput);
+  changeDisplay(btnSubmit, true);
+  changeDisplay(sectionCashAmountInput, false);
+  changeDisplay(sectionCashReturnOutput, false);
+
+  billAmount.focus();
+
 }
