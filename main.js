7b1489a36668c63307c562cd6343a89036df2dac
Bharati Subramanian <bharatisharada@gmail.com>
2021-08-08 14:39:52 +0530

    update: calculate return notes

--- main.js
+++ main.js
@@ -3,19 +3,7 @@
 const billAmount = document.getElementById('bill-amount');
 const cashAmount = document.getElementById("cash-amount");
 const message = document.createElement('p');
-message.className = 'hidden';
-
-
-// const twoThousandNotes = document.querySelector('.two-thousand');
-// const fiveHundredNotes = document.querySelector('.five-hundred');
-// const hundredNotes = document.querySelector('.hundred');
-// const twentyNotes = document.querySelector('.twenty');
-// const tenNotes = document.querySelector('.ten');
-// const fiveNotes = document.querySelector('.five');
-// const oneCoin = document.querySelector('.one');
-
 const numNotesDiv = document.querySelectorAll('.num-notes');
-
 
 const sectionBillAmount = document.querySelector('.bill-amount-input');
 const sectionCashAmountInput = document.querySelector('.cash-amount-input');
@@ -25,12 +13,14 @@ const btnSubmit = document.querySelector('.btn-submit');
 const btnCashSubmit = document.querySelector('.btn-submit-cash')
 const btnReset = document.querySelector('.btn-reset')
 
+message.className = 'hidden';
 const notes = [2000, 500, 100, 20, 10, 5, 1];
 
 billAmount.focus();
 
 // Handle bill amount submit click
 btnSubmit.addEventListener('click', handleBillAmountSubmit);
+
 function handleBillAmountSubmit(e) {
     // Save bill amount
     const billAmountValue = billAmount.value;
@@ -41,18 +31,18 @@ function handleBillAmountSubmit(e) {
     }
 
     // Check if bill amount is negative or 0 
-    else if (parseInt(billAmountValue) <= 0) {
-      displayErrorMessage( true, "Please enter a valid bill amount", sectionBillAmount);
+    else if (Number(billAmountValue) <= 0) {
+        displayErrorMessage( true, "Please enter a valid bill amount", sectionBillAmount);
     } 
 
     // If bill amount is valid remove error messages, submit button, and show the cash input fields.
     else {
-      displayErrorMessage(false, "", sectionBillAmount);
+        displayErrorMessage(false, "", sectionBillAmount);
 
-      changeDisplay(btnSubmit, false);
-      changeDisplay(sectionCashAmountInput, true);
+        changeDisplay(btnSubmit, false);
+        changeDisplay(sectionCashAmountInput, true);
 
-      cashAmount.focus();
+        cashAmount.focus();
     }
 }
 
@@ -65,11 +55,11 @@ function handleCashSubmit(e) {
 
     // Check if cash amount or bill amount is empty
     if ( cashAmountInput === "" || billAmountValue === "" || cashAmountInput === null || billAmountValue === null) {
-      displaySuccessMessage(false, "", sectionCashAmountInput);
+        displaySuccessMessage(false, "", sectionCashAmountInput);
 
-      displayErrorMessage( true, "Please enter a valid bill and cash amount to continue", sectionCashAmountInput);
+        displayErrorMessage( true, "Please enter valid bill and cash amount to continue", sectionCashAmountInput);
 
-      changeDisplay(sectionCashReturnOutput, false);
+        changeDisplay(sectionCashReturnOutput, false);
     } 
 
     // Check if bill amount or cash amount is negative or 0
@@ -91,9 +81,9 @@ function handleCashSubmit(e) {
 
         // IF cash paid is less than bill amount, show error message.
         if (cash < bill) {
-          displaySuccessMessage(false, "", sectionCashAmountInput);
+            displaySuccessMessage(false, "", sectionCashAmountInput);
 
-          displayErrorMessage(true, "Cash paid is less than bill. Please enter the right amount",sectionCashAmountInput);
+            displayErrorMessage(true, "Cash paid is less than bill. Please enter the right amount",sectionCashAmountInput);
         } 
         
         // If cash paid is equal to bill then show success message
@@ -111,8 +101,8 @@ function handleCashSubmit(e) {
 
             clearNumNotesDiv();
             calculateNumNotes(cash, bill);
-          }
-      }
+        }
+    }
 }
 
 
@@ -193,7 +183,8 @@ function calculateNumNotes(cash, bill) {
     for(let i = 0; i<notes.length; i++) {
         if(change >= notes[i]) {
             numNotes[i] = parseInt(change / notes[i])
-            change-= numNotes[i] * notes[i];
+            // change-= numNotes[i] * notes[i];
+            change = change % notes[i];
         }
         else {
             numNotes[i] = "";
@@ -203,67 +194,11 @@ function calculateNumNotes(cash, bill) {
 
     console.log(numNotes)
     
-        /*
-          if (change >= 2000) {
-              numNotes[0] = parseInt(change / 2000);
-              change -= 2000 * numNotes[0];
-          }
-
-          if (change >= 500) {
-              numNotes[1] = parseInt(change / 500);
-              change -= 500 * numNotes[1];
-          }
-
-          if (change >= 100) {
-              numNotes[2] = parseInt(change / 100);
-              change -= 100 * numNotes[2];
-          }
-
-          if (change >= 20) {
-              numNotes[3] = parseInt(change / 20);
-              change -= 20 * numNotes[3];
-          }
-
-          if (change >= 10) {
-              numNotes[4] = parseInt(change / 10);
-              change -= 10 * numNotes[4];
-          }
-
-          if (change >= 5) {
-              numNotes[5] = parseInt(change / 5);
-              change -= 5 * numNotes[5];
-          }
-
-          if (change >= 1) {
-              numNotes[6] = parseInt(change / 1);
-              change -= 1 * numNotes[6];
-          }
-
-
-          twoThousandNotes.textContent = numNotes[0];
-          fiveHundredNotes.textContent = numNotes[1];
-          hundredNotes.textContent = numNotes[2];
-          twentyNotes.textContent = numNotes[3];
-          tenNotes.textContent = numNotes[4];
-          fiveNotes.textContent = numNotes[5];
-          oneCoin.textContent = numNotes[6];
-      */
 }
 
 
-// function to clear the num notes div
 function clearNumNotesDiv() {
-    /*
-        twoThousandNotes.textContent = "";
-        fiveHundredNotes.textContent = "";
-        hundredNotes.textContent = "";
-        twentyNotes.textContent = "";
-        tenNotes.textContent = "";
-        fiveNotes.textContent = "";
-        oneCoin.textContent = "";
-    */
-
     for(let i = 0; i<numNotesDiv.length; i++) {
-      numNotesDiv[i].innerHTML = "";
+        numNotesDiv[i].innerHTML = "";
     }
 }
